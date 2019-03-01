80542ad5752d6e35273d05932af61c5d395b3b5b
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-01 14:30:15 -0800

    Made author and quote fields required for submission

--- submit-quote.php
+++ submit-quote.php
@@ -20,8 +20,8 @@ get_header(); ?>
 					<?php if(is_user_logged_in()&&current_user_can('edit_posts')):?>
 						<form id="submit-form">
 							<label>Author of Quote</label>
-							<input type="text" name="form-author" id="form-author" />
-							<label>Quote</label> <textarea rows="4" cols="5" id="form-text"></textarea>
+							<input type="text" name="form-author" id="form-author" required />
+							<label>Quote</label> <textarea id="form-text" required></textarea>
 							<label>Where did you find this quote? (e.g. book name)</label> <input type="text" name="form-source" id="form-source" />
 							<label>Provide the URL of the quote source, if availiable</label> <input type="url" name="form-source-url" id="form-source-url" />
 							<input type="submit" value="Submit Quote">
