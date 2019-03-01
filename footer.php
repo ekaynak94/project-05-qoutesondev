de55896230ccc1ccee01bee1e8c797469914c9d3
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-01 14:27:42 -0800

    Refactor for the markup of navigation menu

--- footer.php
+++ footer.php
@@ -11,8 +11,7 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<nav id="site-navigation" class="footer-navigation" role="navigation">
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
 					<p>Brought to you by <a href="https://redacademy.com/">RED Academy</a></p>
