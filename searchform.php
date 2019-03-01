05426cbeb34bc301369da9d8aa0a2350ec1fa1af
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-01 10:05:30 -0800

    Addition of icon for search button

--- searchform.php
+++ searchform.php
@@ -4,7 +4,12 @@
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
+			<span class="icon-search" aria-hidden="true">
+				<i class="fas fa-search"></i>
+			</span>
+			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		</button>
 	</fieldset>
 </form>
+
+
